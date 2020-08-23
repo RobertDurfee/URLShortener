ea58f4de9e2a601656637247dce90f71745d6173
Robert Durfee <robert.durfee.17@gmail.com>
2020-08-23 12:18:36 -0500

    use existing api

--- web/main.js
+++ web/main.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const fs = require('fs')
 const http = require('http')
+const https = require('https')
 const MongoClient = require('mongodb').MongoClient
 const uuid = require('uuid').v4
 
@@ -7,24 +9,22 @@ const uuid = require('uuid').v4
 // Global Constants
 ////////////////////////////////////////////////////////////////////////////////
 
-const MONGO_URL = 'mongodb://localhost:27017'
-const URLS_DB = 'urlsDB'
-const URLS = 'urls'
+const URLS_API_HOSTNAME = 'api.urls.durfee.io'
+const URLS_API_PORT = 443
+const URLS_RESOURCE_BASE_URI = '/urls'
+const URLS_CA = fs.readFileSync('../ca.cert.pem')
+const STATUS_CODE_OK = 200
 const STATUS_CODE_FOUND = 302
 const STATUS_CODE_NOT_FOUND = 404
 const STATUS_CODE_INTERNAL_SERVER_ERROR = 500
+const STATUS_CODE_PARSE_ERROR = 601
 const STATUS_NOT_FOUND = 'NOT_FOUND'
 const STATUS_INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR'
+const STATUS_PARSE_ERROR = 'PARSE_ERROR'
 const LOG_OPTIONS = {
     'color': true,
     'depth': null,
 }
-
-////////////////////////////////////////////////////////////////////////////////
-// Global Variables
-////////////////////////////////////////////////////////////////////////////////
-
-var urlsDB
 
 ////////////////////////////////////////////////////////////////////////////////
 // Logging Functions
@@ -55,6 +55,55 @@ const logResponse = (id, res, body) => {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// HTTPS Request Wrappers
+////////////////////////////////////////////////////////////////////////////////
+
+const httpsGet = (hostname, port, uri) => {
+    const options = {
+        'hostname': hostname,
+        'port': port,
+        'path': uri,
+        'method': 'GET',
+        'ca': URLS_CA,
+    }
+    return new Promise((resolve, reject) => {
+        const req = https.request(options, res => {
+            var body = ''
+            res.on('data', data => {
+                body = body + data
+            })
+            res.on('end', () => {
+                resolve = (res.statusCode == STATUS_CODE_OK) ? resolve : reject
+                if (body == '') {
+                    resolve({})
+                } else {
+                    try {
+                        body = JSON.parse(body)
+                        resolve(body)
+                    } catch (error) {
+                        reject({
+                            'error': {
+                                'code': STATUS_CODE_PARSE_ERROR,
+                                'message': `Unable to parse: ${error.message}: ${body}`,
+                                'status': STATUS_PARSE_ERROR,
+                            }
+                        })
+                    }
+                }
+            })
+        })
+    })
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// API Wrappers
+////////////////////////////////////////////////////////////////////////////////
+
+const urlsGet = resourceId => {
+    return httpsGet(URLS_API_HOSTNAME, URLS_API_PORT, `${URLS_RESOURCE_BASE_URI}/${resourceId}`)
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // App Configuration
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -71,37 +120,16 @@ app.set('json spaces', 2)
 app.get('/:resourceId', (req, res) => {
     const id = uuid()
     logRequest(id, req)
-    urlsDB.collection(URLS).findOne({
-        'id': req.params['resourceId'].toLowerCase()
-    }).then(result => {
-        if (result) {
-            const body = {
-                'id': `${result['id']}`,
-                'url': `${result['url']}`
-            }
-            res.status(STATUS_CODE_FOUND).set('Location', `${result['url']}`).json(body)
-            logResponse(id, res, body)
-        } else {
-            const body = {
-                'error': {
-                    'code': STATUS_CODE_NOT_FOUND,
-                    'message': `Resource ${req.params['resourceId']} was not found.`,
-                    'status': STATUS_NOT_FOUND,
-                }
-            }
-            res.status(STATUS_CODE_NOT_FOUND).json(body)
-            logResponse(id, res, body)
-        }
-    }).catch(error => {
+    urlsGet(req.params['resourceId']).then(result => {
         const body = {
-            'error': {
-                'code': STATUS_CODE_INTERNAL_SERVER_ERROR,
-                'message': `Unexpected error occurred when getting resource ${req.params['resourceId']}: ${error.message}`,
-                'status': STATUS_INTERNAL_SERVER_ERROR
-            }
+            'id': `${result['id']}`,
+            'url': `${result['url']}`
         }
-        res.status(STATUS_CODE_INTERNAL_SERVER_ERROR).json(body)
+        res.status(STATUS_CODE_FOUND).set('Location', `${result['url']}`).json(body)
         logResponse(id, res, body)
+    }).catch(error => {
+        res.status(STATUS_CODE_INTERNAL_SERVER_ERROR).json(error)
+        logResponse(id, res, error)
     })
 })
 
@@ -109,10 +137,4 @@ app.get('/:resourceId', (req, res) => {
 // Connect
 ////////////////////////////////////////////////////////////////////////////////
 
-MongoClient.connect(MONGO_URL, { useUnifiedTopology: true }).then(client => {
-    urlsDB = client.db(URLS_DB)
-    http.createServer(app).listen(8004)
-}).catch(error => {
-    console.error(error)
-    process.exit(1)
-})
+http.createServer(app).listen(8004)
