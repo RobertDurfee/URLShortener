9a5ea0d5ce8ceb882de8c54546f1b64e0215b80a
Robert Durfee <robert@durfee.io>
2020-08-23 22:55:06 -0500

    url validation

--- api/main.js
+++ api/main.js
@@ -13,8 +13,10 @@ const URLS_DB = 'urlsDB'
 const URLS = 'urls'
 const URLS_COUNT = 'urls.count'
 const STATUS_CODE_OK = 200
+const STATUS_CODE_BAD_REQUEST = 400
 const STATUS_CODE_NOT_FOUND = 404
 const STATUS_CODE_INTERNAL_SERVER_ERROR = 500
+const STATUS_BAD_REQUEST = 'BAD_REQUEST'
 const STATUS_NOT_FOUND = 'NOT_FOUND'
 const STATUS_INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR'
 const LOG_OPTIONS = {
@@ -129,39 +131,51 @@ app.get('/urls/:resourceId', (req, res) => {
 app.post('/urls', (req, res) => {
     const id = uuid()
     logRequest(id, req)
-    urlsDB.collection(URLS_COUNT).findOneAndUpdate({
-        '_id': 'count',
-    }, {
-        '$inc': {
-            'count': 1
-        }
-    }).then(result => {
-        if (result.value) {
-            return urlsDB.collection(URLS).insertOne({
-                'id': `${convert(result.value['count'])}`,
-                'url': req.body['url'],
-            })
-        } else { throw new Error('Unable to get urls count') }
-    }).then(result => {
-        if (result.insertedCount == 1) {
-            const body = {
-                'id': `${result.ops[0]['id']}`,
-                'url': `${result.ops[0]['url']}`,
-            }
-            res.status(STATUS_CODE_OK).json(body)
-            logResponse(id, res, body)
-        } else { throw new Error(`Unexpected number of inserted resources: ${result.insertedCount}`) }
-    }).catch(error => {
+    if (!req.body['url'] || !/^https?:\/\/([A-Za-z0-9-]{1,63}\.)+[A-Za-z]{2,6}(\/([-a-zA-Z0-9()@:%_\+.~#?&\/=]*))?$/.test(req.body['url'])) {
         const body = {
             'error': {
-                'code': STATUS_CODE_INTERNAL_SERVER_ERROR,
-                'message': `Unexpected error occurred when inserting resource: ${error.message}`,
-                'status': STATUS_INTERNAL_SERVER_ERROR,
+                'code': STATUS_CODE_BAD_REQUEST,
+                'message': `Provided URL is not valid: ${req.body['url']}`,
+                'status': STATUS_BAD_REQUEST,
             }
         }
-        res.status(STATUS_CODE_INTERNAL_SERVER_ERROR).json(body)
+        res.status(STATUS_CODE_BAD_REQUEST).json(body)
         logResponse(id, res, body)
-    })
+    } else {
+        urlsDB.collection(URLS_COUNT).findOneAndUpdate({
+            '_id': 'count',
+        }, {
+            '$inc': {
+                'count': 1
+            }
+        }).then(result => {
+            if (result.value) {
+                return urlsDB.collection(URLS).insertOne({
+                    'id': `${convert(result.value['count'])}`,
+                    'url': req.body['url'],
+                })
+            } else { throw new Error('Unable to get urls count') }
+        }).then(result => {
+            if (result.insertedCount == 1) {
+                const body = {
+                    'id': `${result.ops[0]['id']}`,
+                    'url': `${result.ops[0]['url']}`,
+                }
+                res.status(STATUS_CODE_OK).json(body)
+                logResponse(id, res, body)
+            } else { throw new Error(`Unexpected number of inserted resources: ${result.insertedCount}`) }
+        }).catch(error => {
+            const body = {
+                'error': {
+                    'code': STATUS_CODE_INTERNAL_SERVER_ERROR,
+                    'message': `Unexpected error occurred when inserting resource: ${error.message}`,
+                    'status': STATUS_INTERNAL_SERVER_ERROR,
+                }
+            }
+            res.status(STATUS_CODE_INTERNAL_SERVER_ERROR).json(body)
+            logResponse(id, res, body)
+        })
+    }
 })
 
 ////////////////////////////////////////////////////////////////////////////////
